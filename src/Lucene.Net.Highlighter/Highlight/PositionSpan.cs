9f8133fd56e2d6479b77cc0aaecaf58a4d33a714
Joshua Ball <joshua.g.s.ball@gmail.com>
2015-01-26 23:32:27 -0800

    And ssome more.

--- src/Lucene.Net.Highlighter/Highlight/PositionSpan.cs
+++ src/Lucene.Net.Highlighter/Highlight/PositionSpan.cs
@@ -1,23 +1,17 @@
-/*
- * This code is derived from MyJavaLibrary (http://somelinktomycoollibrary)
- * 
- * If this is an open source Java library, include the proper license and copyright attributions here!
- */
-
 namespace Lucene.Net.Search.Highlight
 {
-	/// <summary>Utility class to record Positions Spans</summary>
-	/// <lucene.internal></lucene.internal>
-	public class PositionSpan
-	{
-		internal int start;
+    /// <summary>Utility class to record Positions Spans</summary>
+    /// <lucene.internal></lucene.internal>
+    public class PositionSpan
+    {
+        internal int start;
 
-		internal int end;
+        internal int end;
 
-		public PositionSpan(int start, int end)
-		{
-			this.start = start;
-			this.end = end;
-		}
-	}
+        public PositionSpan(int start, int end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+    }
 }
